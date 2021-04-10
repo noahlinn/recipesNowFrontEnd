27b3d8093e64c3807c98daa4e7ef32cff4c3f103
noahlinn <n.linn95@gmail.com>
2021-04-10 18:50:43 -0400

    getting profile set up

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
+
 const loginOrSignUp = document.querySelector(".login-or-signup")
 const signUpButton = document.querySelector("#signup-button")
 const loginButton = document.querySelector("#login-button")
 const signUpPage = document.querySelector(".signup")
 const sections = document.querySelectorAll('section')
 const signUpForm = document.querySelector('.signup-form')
-const logOutButton = document.querySelector('.log-out')
+const logOutButtons = document.querySelectorAll('.log-out')
 const searchPage = document.querySelector('.search-page')
 const loginPage = document.querySelector('.login')
 const loginForm = document.querySelector('.login-form')
 const searchForm = document.querySelector('.search-form')
 const searchResults = document.querySelector('.search-results')
 const recipeDeatilsPage = document.querySelector('.recipe-details')
+const saveButton = document.querySelector('#save-recipe')
+const backToResusltsButton = document.querySelector('#back-to-results')
+const resultCard = document.querySelector('.result-card')
+const profileButton = document.querySelector('.profile-button')
+const profilePage = document.querySelector('.profile-page')
+let recipeId = null
 
 signUpButton.addEventListener('click', () => {
     console.log('click')
@@ -23,33 +30,54 @@ loginButton.addEventListener('click', () => {
     removeHidden(loginPage)
 })
 
-logOutButton.addEventListener('click', () => {
-    localStorage.clear()
-    hideSections()
-    removeHidden(loginOrSignUp)
-})
+for (const button of logOutButtons) {
+    button.addEventListener('click', () => {
+        localStorage.clear()
+        hideSections()
+        removeHidden(loginOrSignUp)
+    })
+}
 
 signUpForm.addEventListener('submit', async (e) => {
     e.preventDefault()
     signUp()
-    clearSearchResults()
+    clearResults(searchResults)
 })
 
 loginForm.addEventListener('submit', async (e) => {
     e.preventDefault()
     login()
-    clearSearchResults()   
+    clearResults(searchResults)
 })
 
-searchForm.addEventListener('submit',  (e) => {
+saveButton.addEventListener('click', () => {
+    console.log('click')
+    const userId = localStorage.getItem('userId')
+    console.log(userId, recipeId)
+    saveRecipe(userId, recipeId)
+})
+
+searchForm.addEventListener('submit', (e) => {
     e.preventDefault()
     const query = document.querySelector('#search-recipes').value
     const id = localStorage.getItem('userId')
-    clearSearchResults()
+    clearResults(searchResults)
     getRecipes(id, query)
 })
 
-getRecipes =  async (id, query) => {
+backToResusltsButton.addEventListener('click', () => {
+    addHidden(recipeDeatilsPage)
+    removeHidden(searchPage)
+})
+
+profileButton.addEventListener('click', () => {
+    hideSections()
+    removeHidden(profilePage)
+    console.log('click')
+    showSavedRecipe()
+})
+
+getRecipes = async (id, query) => {
     try {
         const res = await axios.post('http://localhost:3001/recipes/search', {
             id: id,
@@ -57,17 +85,18 @@ getRecipes =  async (id, query) => {
         })
         console.log(res)
         let resultsArray = res.data.results
-        if (resultsArray.length === 0){
+        if (resultsArray.length === 0) {
             alert("No results found")
         }
-        else{
-        for (let result of resultsArray) {
-            let recipeName = result.title
-            let recipeImage = result.image
-            let recipeId = result.id
-            displayRecipes(recipeId, recipeName, recipeImage)
+        else {
+            for (let result of resultsArray) {
+                let recipeName = result.title
+                let recipeImage = result.image
+                let recipeId = result.id
+                displayRecipes(recipeId, recipeName, recipeImage)
+            }
         }
-    }}
+    }
     catch (error) {
         alert(error)
     }
@@ -83,40 +112,50 @@ displayRecipes = (id, name, img) => {
     card.append(recipeName, recipeImage)
     searchResults.append(card)
     card.addEventListener('click', () => {
+        recipeId = id
         console.log(id)
         displaySingle(id)
-        
+
     })
 }
 
 displaySingle = async (recipeId) => {
     try {
-        hideSections()
+        addHidden(searchPage)
         removeHidden(recipeDeatilsPage)
+        clearResults(resultCard)
         let results = await axios.get(`http://localhost:3001/recipes/search/${recipeId}`)
         createRecipeElements(results)
     } catch (error) {
-        
+
     }
 }
 createRecipeElements = (results) => {
     let recipeHeader = document.createElement('h1')
+    let recipeImg = document.createElement('img')
+    recipeImg.src = results.data.image
+    let instructionHeader = document.createElement('h3')
+    instructionHeader.innerHTML = 'Instructions:'
+    let ingredientHeader = document.createElement('h3')
+    ingredientHeader.innerHTML = 'Ingredients:'
     let recipeInstructions = document.createElement('p')
     let ingredientList = document.createElement('ul')
     recipeHeader.innerText = results.data.title
     recipeInstructions.innerHTML = results.data.instructions
     let ingredients = results.data.extendedIngredients
-    for(let ingredient of ingredients){
+    for (let ingredient of ingredients) {
         let eachIngredient = document.createElement('li')
         eachIngredient.innerHTML = ingredient.original
         ingredientList.append(eachIngredient)
     }
-    recipeDeatilsPage.append(recipeHeader, recipeInstructions, ingredientList)
+    resultCard.append(recipeHeader, recipeImg, instructionHeader,
+        recipeInstructions, ingredientHeader, ingredientList)
 }
 
-clearSearchResults = () => {
-    while (searchResults.firstChild) {
-        searchResults.firstChild.remove()
+
+clearResults = (result) => {
+    while (result.firstChild) {
+        result.firstChild.remove()
     }
 }
 
@@ -126,6 +165,10 @@ hideSections = () => {
 
 removeHidden = (section) => {
     section.classList.remove('hidden')
+}
+
+addHidden = (section) => {
+    section.classList.add('hidden')
 }
 
 signUp = async () => {
@@ -165,6 +208,25 @@ login = async () => {
         alert('login failed')
     }
 }
+saveRecipe = async (userId, recipeId) => {
+    try {
+        let res = await axios.post(`http://localhost:3001/users/${userId}/save/${recipeId}`)
+        alert('Recipe Saved')
+    } catch (error) {
+        alert('save failed')
+    }
+}
+
+showSavedRecipe = async () => {
+    try {
+        const userId = localStorage.getItem('userId')
+        let res = await axios.get(`http://localhost:3001/users/${userId}/savedRecipes`)
+        console.log(res.data)
+    } catch (error) {
+        
+    }
+}
+
 
 if (localStorage.getItem('userId')) {
     hideSections()
