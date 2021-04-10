6af457574d77edee46869809e86b045351f41914
noahlinn <n.linn95@gmail.com>
2021-04-10 15:59:32 -0400

    display individual recipes on click

--- main.js
+++ main.js
@@ -4,33 +4,135 @@ const loginButton = document.querySelector("#login-button")
 const signUpPage = document.querySelector(".signup")
 const sections = document.querySelectorAll('section')
 const signUpForm = document.querySelector('.signup-form')
-
+const logOutButton = document.querySelector('.log-out')
 const searchPage = document.querySelector('.search-page')
 const loginPage = document.querySelector('.login')
 const loginForm = document.querySelector('.login-form')
+const searchForm = document.querySelector('.search-form')
+const searchResults = document.querySelector('.search-results')
+const recipeDeatilsPage = document.querySelector('.recipe-details')
 
-hideSections = () => {
-    sections.forEach(section => section.classList.add('hidden'))
-}
-removeHidden = (section) => {
-    section.classList.remove('hidden')
-}
 signUpButton.addEventListener('click', () => {
     console.log('click')
     hideSections()
     removeHidden(signUpPage)
 })
+
 loginButton.addEventListener('click', () => {
     hideSections()
     removeHidden(loginPage)
 })
+
+logOutButton.addEventListener('click', () => {
+    localStorage.clear()
+    hideSections()
+    removeHidden(loginOrSignUp)
+})
+
 signUpForm.addEventListener('submit', async (e) => {
     e.preventDefault()
+    signUp()
+    clearSearchResults()
+})
+
+loginForm.addEventListener('submit', async (e) => {
+    e.preventDefault()
+    login()
+    clearSearchResults()   
+})
+
+searchForm.addEventListener('submit',  (e) => {
+    e.preventDefault()
+    const query = document.querySelector('#search-recipes').value
+    const id = localStorage.getItem('userId')
+    clearSearchResults()
+    getRecipes(id, query)
+})
+
+getRecipes =  async (id, query) => {
+    try {
+        const res = await axios.post('http://localhost:3001/recipes/search', {
+            id: id,
+            query: query
+        })
+        console.log(res)
+        let resultsArray = res.data.results
+        if (resultsArray.length === 0){
+            alert("No results found")
+        }
+        else{
+        for (let result of resultsArray) {
+            let recipeName = result.title
+            let recipeImage = result.image
+            let recipeId = result.id
+            displayRecipes(recipeId, recipeName, recipeImage)
+        }
+    }}
+    catch (error) {
+        alert(error)
+    }
+}
+
+displayRecipes = (id, name, img) => {
+    let card = document.createElement('div')
+    let recipeImage = document.createElement('img')
+    let recipeName = document.createElement('p')
+    card.classList.add('recipe-card')
+    recipeName.innerText = name
+    recipeImage.src = img
+    card.append(recipeName, recipeImage)
+    searchResults.append(card)
+    card.addEventListener('click', () => {
+        console.log(id)
+        displaySingle(id)
+        
+    })
+}
+
+displaySingle = async (recipeId) => {
+    try {
+        hideSections()
+        removeHidden(recipeDeatilsPage)
+        let results = await axios.get(`http://localhost:3001/recipes/search/${recipeId}`)
+        createRecipeElements(results)
+    } catch (error) {
+        
+    }
+}
+createRecipeElements = (results) => {
+    let recipeHeader = document.createElement('h1')
+    let recipeInstructions = document.createElement('p')
+    let ingredientList = document.createElement('ul')
+    recipeHeader.innerText = results.data.title
+    recipeInstructions.innerHTML = results.data.instructions
+    let ingredients = results.data.extendedIngredients
+    for(let ingredient of ingredients){
+        let eachIngredient = document.createElement('li')
+        eachIngredient.innerHTML = ingredient.original
+        ingredientList.append(eachIngredient)
+    }
+    recipeDeatilsPage.append(recipeHeader, recipeInstructions, ingredientList)
+}
+
+clearSearchResults = () => {
+    while (searchResults.firstChild) {
+        searchResults.firstChild.remove()
+    }
+}
+
+hideSections = () => {
+    sections.forEach(section => section.classList.add('hidden'))
+}
+
+removeHidden = (section) => {
+    section.classList.remove('hidden')
+}
+
+signUp = async () => {
     const name = document.querySelector('#signup-name').value
     const email = document.querySelector('#signup-email').value
     const password = document.querySelector('#signup-password').value
     const diet = document.querySelector('#diet').value
-    // console.log(name, email, password, diet)
     try {
         let res = await axios.post('http://localhost:3001/users', {
             name: name,
@@ -45,18 +147,16 @@ signUpForm.addEventListener('submit', async (e) => {
     } catch (error) {
         alert('email already exists or invalid')
     }
-})
+}
 
-loginForm.addEventListener('submit', async (e) => {
-    e.preventDefault()
+login = async () => {
     const email = document.querySelector('#login-email').value
     const password = document.querySelector('#login-password').value
-    console.log(email, password)
     try {
         let res = await axios.post('http://localhost:3001/users/login', {
             email: email,
             password: password
-        })  
+        })
         let userId = res.data.user.id
         localStorage.setItem('userId', userId)
         hideSections()
@@ -64,5 +164,13 @@ loginForm.addEventListener('submit', async (e) => {
     } catch (error) {
         alert('login failed')
     }
-    
-})
+}
+
+if (localStorage.getItem('userId')) {
+    hideSections()
+    removeHidden(searchPage)
+}
+else {
+    hideSections()
+    removeHidden(loginOrSignUp)
+}
